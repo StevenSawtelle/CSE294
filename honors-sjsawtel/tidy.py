bba095d593373e5a6494261bf71f89b5a3f710cb
StevenSawtelle <steven.sawtelle@gmail.com>
2017-05-04 00:53:31 -0700

    init optional assign, test remote repo

---
+++ honors-sjsawtel/tidy.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+#name: Steven Sawtelle
+#CSE 294
+#started: 5-4-17
+"""
+program shall take in a given amount of numbers, and then
+for that many numbers determine if it is "tidy" - that is,
+if the numbers from left to right are (not strictly) increasing
+ie)
+129 is tidy
+132 is not tidy
+"""
+
+import sys
+
+def main():
+	
+
+#never forget this lil dude!
+if __name__ == "__main__":
+    main()
