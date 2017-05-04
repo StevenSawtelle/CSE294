eec0c55f28e2b5c963d8ee33de699a1396186154
StevenSawtelle <steven.sawtelle@gmail.com>
2017-05-03 23:52:50 -0700

    updating maxprofits algo

--- final-sjsawtel/maxprofit.py
+++ final-sjsawtel/maxprofit.py
@@ -2,55 +2,60 @@
 
 import sys
 
-#file: maxprofit.py
-#Steven Sawtelle
 
-#program goes through a list of given inputs starting with an expected max profit
-#it returns the max profit that can be made using the given inputs
-#and whether or not that matches the expected max profit. runs in O() time
+# file: maxprofit.py
+# Steven Sawtelle
+
+# program goes through a list of given inputs starting with an expected max profit
+# it returns the max profit that can be made using the given inputs
+# and whether or not that matches the expected max profit. runs in O() time
 
 def process(prices):
-    #default vals to be tracked
-    minSoFar=prices[0]
+    # default vals to be tracked
+    minSoFar = prices[0]
     currentMax = prices[0]
-    maxProfit =0
-    #iterate through each element
+    maxProfit = 0
+    tempMin = prices[0]
+    # iterate through each element
     for x in prices:
-        #if this element is the min, make it so in the code!
-        if x<minSoFar:
-            minSoFar = x
-        #otherwise see if selling now is the best band for ya buck
+        # if this element is the min, make it so in the code!
+        if x < tempMin:
+            # might want this price later
+            tempMin = x
+        # otherwise see if selling now is the best bang for ya buck
         else:
-            temp = x-minSoFar
-            #if it is, make it so in the code!
+            temp = x - tempMin
+            # if it is, make it so in the code!
             if temp > maxProfit:
                 maxProfit = temp
                 currentMax = x
-    #return them all. round maxProfit cuz python likes to make it ugly
+                minSoFar = tempMin
+    # return them all. round maxProfit cuz python likes to make it ugly
     return (round(maxProfit, 2), minSoFar, currentMax)
 
+
 def main():
-    count=1
-    #read all lines from stdin
+    count = 1
+    # read all lines from stdin
     for line in sys.stdin:
-        #get current line as array of floats
+        # get current line as array of floats
         prices = [float(x) for x in line.split()]
-        #no accepted way to handle lists of 1 or less, so just ignore them
-        #janky, but works for the given problem
-        if len(prices)>1:
-            #get values by splitting the first element from a list
+        # no accepted way to handle lists of 1 or less, so just ignore them
+        # janky, but works for the given problem
+        if len(prices) > 1:
+            # get values by splitting the first element from a list
             expectedCost = prices[0]
             prices = prices[1:]
-            #gets back relevant values for printing nicely
+            # gets back relevant values for printing nicely
             (actualCost, actualMin, actualMax) = process(prices)
-            #write out corresponding pass or fail message
-            if actualCost==expectedCost:
-                sys.stdout.write("Running test case "+str(count) + "\n"
-                                 + "Expected: Max profit is " + str(expectedCost) +"\n"
-                                 + "Actual: Buy at "+ str(actualMin)
-                                 +", Sell at "+ str(actualMax)
-                                 +", Max profit is "+ str(actualCost) +"\n"
-                                 +"Test case passed\n");
+            # write out corresponding pass or fail message
+            if actualCost == expectedCost:
+                sys.stdout.write("Running test case " + str(count) + "\n"
+                                 + "Expected: Max profit is " + str(expectedCost) + "\n"
+                                 + "Actual: Buy at " + str(actualMin)
+                                 + ", Sell at " + str(actualMax)
+                                 + ", Max profit is " + str(actualCost) + "\n"
+                                 + "Test case passed\n");
             else:
                 sys.stdout.write("Running test case " + str(count) + "\n"
                                  + "Expected: Max profit is " + str(expectedCost) + "\n"
@@ -58,10 +63,10 @@ def main():
                                  + ", Sell at " + str(actualMax)
                                  + ", Max profit is " + str(actualCost) + "\n"
                                  + "Test case FAILED\n");
-        #increment count
-        count+=1
+        # increment count
+        count += 1
 
 
-#ever-present little driver
+# ever-present little driver
 if __name__ == "__main__":
     main()
